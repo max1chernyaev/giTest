c18975b08e3629a89a4085f5f0da200a865cfd03
max1chernyaev <max1chernyaev@gmail.com>
2021-11-15 11:57:30 +0200

    Добавлен "user3"

--- src/gitTestMain.java
+++ src/gitTestMain.java
@@ -7,6 +7,7 @@ public class gitTestMain extends gitTest{
 
         gitTest user1 = new gitTest("Max", "Chernyaev", 29);
         gitTest user2 = new gitTest("Jack", "Black", 60);
+        gitTest user3 = new gitTest("Sam", "Wilson", 45);
 
         System.out.println(user1);
 
